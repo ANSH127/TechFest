09d8cbd4c7d1d803fb5c9ef7b2e0744eedf613ea
Ansh Agarwal <anshagrawal12348@gmail.com>
2023-10-07 17:38:08 +0530

    sponser layout

--- src/pages/Sponser.js
+++ src/pages/Sponser.js
@@ -1,7 +1,55 @@
-import React from 'react'
+import React from 'react';
+import { Container, Grid, Typography } from '@mui/material';
 
-export default function Sponser() {
+
+const Sponsor = () => {
+
   return (
-    <div>Sponser</div>
-  )
-}
+    <Container maxWidth="lg" 
+    sx={{
+      marginTop:'20px',
+      marginBottom:'20px',
+    }}
+    >
+      <Typography variant="h4" gutterBottom>
+        Our Sponsors
+      </Typography>
+      <Grid container spacing={3}>
+        <Grid item xs={12} sm={6} md={4}>
+          <img
+            src="https://via.placeholder.com/300x150"
+            alt="sponsor"
+            sx={{
+              maxWidth: '100%',
+              height: 'auto',
+            }}
+          />
+        </Grid>
+        <Grid item xs={12} sm={6} md={4}>
+          <img
+            src="https://via.placeholder.com/300x150"
+            alt="sponsor"
+            
+            sx={{
+              maxWidth: '100%',
+              height: 'auto',
+            }}
+          />
+        </Grid>
+        <Grid item xs={12} sm={6} md={4}>
+          <img
+            src="https://via.placeholder.com/300x150"
+            alt="sponsor"
+            
+            sx={{
+              maxWidth: '100%',
+              height: 'auto',
+            }}
+          />
+        </Grid>
+      </Grid>
+    </Container>
+  );
+};
+
+export default Sponsor;
